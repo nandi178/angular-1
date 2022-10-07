6ad24443238157b3cb1f41943967fc2c5b106c61
Flesser Nándor <flesser.nandor@ujvilag.gov.hu>
2022-10-07 14:46:55 +0200

    3. hét közepe mentés.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,17 @@ export class AppComponent {
   counter2 = {name: 'Ezaz', id: 1};
   aim = 5;
   logoSrc = 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Porsche_992_Carrera_4S_Genf_2019_1Y7A5561.jpg'
+  isComplete = false;
+  
   countChanged(count: number)
   {
     if (count===this.aim) {
+      this.isComplete=true;
       alert('Cél elérve');
+    }
+    if (count === 3) {
+      this.aim = 10;
+      this.counter2 .name =' Név változás';
     }
   }
 }
