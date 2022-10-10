e884cac39aea0566fe4f156c1710079e11ca6fb1
Flesser Nándor <flesser.nandor@ujvilag.gov.hu>
2022-10-10 12:40:30 +0200

    3. hét full.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,8 @@ export class AppComponent {
   aim = 5;
   logoSrc = 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Porsche_992_Carrera_4S_Genf_2019_1Y7A5561.jpg'
   isComplete = false;
+  counterCount = 2;
+  counters: {name: string, id: number, aim: number}[] = [];
   
   countChanged(count: number)
   {
@@ -23,4 +25,24 @@ export class AppComponent {
       this.counter2 .name =' Név változás';
     }
   }
+
+  counterCountChanged() {
+                 //hány számlálónk vann - a felhasználó mennyit szeetne.
+    const diff = this.counterCount - this.counters.length;
+    if (diff > 0) {
+      //mappel végig tudunk iterálni egy tömb elemen és át tudjuk alakítani egy másik formába. _ paraméter a tömb aktuális eleme (ez minket nem érdekel)
+      const newCounters = new Array(diff).fill(undefined).map((_, i) => {
+        return {name: '', id: i + this.counterCount, aim: 10};
+      });
+      this.counters = [...this.counters, ...newCounters]
+    } else if (diff < 0) {
+      this.counters.splice(this.counterCount); //első paraméter, honnan töröljünk. Ha nem adunk meg második paramétert, hogy hány darabot, akkor az első darbtól az összeset törli.
+    }    
+  }
+
+
+//  Értékadás a feliratnak. Már nincs használatban, a html-ben is komment lett. Helyette ngModel.
+//  counterCountChanged(event: any) {
+//    this.counterCount = event.target.value;
+//  }
 }
